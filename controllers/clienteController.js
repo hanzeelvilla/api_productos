d08ae74e8aeda8ea3d5d616a26e6d14ae4353941
Hanzeel Villa <hanzeelvilla0@gmail.com>
2024-05-02 08:19:53 -0600

    POST

--- controllers/clienteController.js
+++ controllers/clienteController.js
@@ -13,25 +13,37 @@ module.exports = {
         })
     },
     buscar:(req, res) => {
-        mysql.query(`select * from clientes where id = ${req.params.id}`, (err, rows, fields) => {
+        mysql.query('select * from clientes where id = ?', req.params.id, (err, rows, fields) => {
             if(err)
             res.json(err)
-            else{
+            else
                 res.json(rows)
-            }
         })
     },
+    agregar:(req, res) => {
+        let nombre = req.body.nombre
+        let rfc = req.body.rfc
+        let celular = req.body.celular
+        let calle = req.body.celular
+        let numero = req.body.numero
+        let codigopostal = req.body.codigopostal
 
+        mysql.query('insert into clientes (nombre, rfc, celular, calle, numero, codigopostal) values (?, ?, ?, ?, ?, ?)', [nombre, rfc, celular, calle, numero, codigopostal], (err, rows, fields) => {
+            if(err)
+                res.json(err)
+            else
+                res.json(rows)
+        })
+    },
     eliminar:(req, res) => {
         let id = req.params.id
-        mysql.query(`delete from clientes where id = ${id}`, (err, rows, fields) => {
+        mysql.query('delete from clientes where id = ?', id, (err, rows, fields) => {
             if(err){
                 console.log(err)
                 res.json(err)
             }
-            else{
+            else
                 res.json(rows)
-            }
         })
     }
 }
--- routes/clientes.js
+++ routes/clientes.js
@@ -9,6 +9,10 @@ router.get("/:id", (req, res) => {
     clienteController.buscar(req, res)
 })
 
+router.post("/", (req, res) => {
+    clienteController.agregar(req, res)
+})
+
 router.delete("/:id", (req, res) => {
     clienteController.eliminar(req, res)
 })
